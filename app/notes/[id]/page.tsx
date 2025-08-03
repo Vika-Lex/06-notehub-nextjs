a0193df84b3fe390b0950d6673ea945fbccf061a
Viktoriia Kolesnik <vika.notariat@ukr.net>
2025-08-03 18:11:45 +0300

    ref HW 06-notehub-nextjs

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,15 +2,13 @@ import {getNoteById} from "@/lib/api";
 import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
 
 interface NotePageProps {
-    params:{
-        id: string
-    }
+    params: Promise<{id: string}>;
 }
 
 const Page = async ({params}: NotePageProps) => {
 
-    // const {id} = params;
-    const note = await getNoteById(params.id)
+    const {id} = await params;
+    const note = await getNoteById(id)
 
     return (
 
